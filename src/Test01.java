a0d00a389ab46bf8aa3a3c63756eccf0765dae92
peniles <lestenary@qq.com>
2021-04-03 11:18:45 +0800

    test

---
+++ src/Test01.java
@@ -0,0 +1,12 @@
+/**
+ * @packageName:PACKAGE_NAME
+ * @className:Test01
+ * @Description:
+ * @author:LJJ
+ * @date:2021/4/3 11:14
+ */
+public class Test01 {
+    public static void main(String args[]){
+        System.out.println("你好");
+    }
+}
